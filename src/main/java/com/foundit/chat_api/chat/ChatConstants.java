83dcdf80132fa728e4f80cc32d6dade0ce0c7049
Tharuka Dananjana <32910424+tharuka95@users.noreply.github.com>
2025-03-13 12:02:56 -0400

    Update ChatConstants.java

--- src/main/java/com/foundit/chat_api/chat/ChatConstants.java
+++ src/main/java/com/foundit/chat_api/chat/ChatConstants.java
@@ -3,7 +3,7 @@ package com.foundit.chat_api.chat;
 public class ChatConstants {
 
     public static final String FIND_CHAT_BY_SENDER_ID = "Chat.findChatsByReceiverId";
-    public static final String FIND_CHAT_BY_SENDER_ID_AND_RECEIVER = "Chat.findChatsByReceiverIdAndReceiver";
+    public static final String FIND_CHAT_BY_LOGIN_USER_ITEM_ID = "Chat.findChatsByLoginUserItemId";
 
     private ChatConstants() {}
 }
