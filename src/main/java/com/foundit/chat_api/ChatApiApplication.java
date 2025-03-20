01dd8d52454f4f7a59254d7a31dc23a1aa175659
Tharuka Dananjana <32910424+tharuka95@users.noreply.github.com>
2025-03-20 12:36:39 -0400

    Update ChatApiApplication.java

--- src/main/java/com/foundit/chat_api/ChatApiApplication.java
+++ src/main/java/com/foundit/chat_api/ChatApiApplication.java
@@ -7,10 +7,12 @@ import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class ChatApiApplication {
 
 	public static void main(String[] args) {
